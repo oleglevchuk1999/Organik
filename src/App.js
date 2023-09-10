34a4f01517bf2e2cc8ab8c7efb7aa9002a235b50
oleglevchuk1999 <o.levghyk@gmail.com>
2023-09-10 15:17:15 +0300

    boots

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
+import Card from "./Componants/CardsMouseFollow/Card";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container" >
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
