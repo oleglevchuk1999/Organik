e7eaed4e6a527cf18bc2c2f3258afc5abe2080d2
oleglevchuk1999 <o.levghyk@gmail.com>
2023-10-16 15:10:31 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Card from "./Componants/CardsMouseFollow/Card";
+
 
 function App() {
   return (
     <div className="App">
       <div className="container" >
-        <Card />
-        <Card />
       </div>
     </div>
   );
